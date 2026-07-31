a990f26f32bc41b18116de98a01b17ac08b43c46
SujitaGopali <your-github-email@example.com>
2026-07-31 11:47:07 +0545

    updated constant

--- backend/src/configs/constant.ts
+++ backend/src/configs/constant.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
 export const PORT = process.env.PORT || "8089";
-export const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/unisphere";
-export const SECRET_KEY = process.env.JWT_SECRET || "unisphere_default_secret_key_12345";
+export const MONGODB_URL =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/class-36a-db";
+export const SECRET_KEY = process.env.SECRET_KEY || "merosecretkey";
